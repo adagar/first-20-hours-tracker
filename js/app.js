dd6d19f9f49cf3664255c388f3d1b0f2592f3370
Nick Garza <adagar121987@gmail.com>
2019-09-15 18:23:05 -0400

    Adding service worker and app javascript file to repo

---
+++ js/app.js
@@ -0,0 +1,5 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js')
+        .then((registration) => console.log("service worker registered", registration))
+        .catch((error) => console.log('service worker NOT registered', error))
+}
