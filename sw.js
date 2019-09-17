9eafef6d1af2043bd66b446318c5dace5699af16
Nick Garza <adagar121987@gmail.com>
2019-09-17 19:20:56 -0400

    15 Precaching URLs being saved to the staticCache

--- sw.js
+++ sw.js
@@ -1,6 +1,24 @@
+const staticCacheName = "site-static"; // the shell, or static resources
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.css',
+    '/img/clock.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+
 // install service worker
 self.addEventListener('install', (evt) => {
     console.log('service worker has been installed', evt);
+    // shell cacheing
+    caches.open(staticCacheName)
+        .then(cache => {
+            cache.addAll()
+        })
 });
 
 // activate event
