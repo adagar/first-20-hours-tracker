9aba770d2a1f011ba4765ddfd8fea5bf88217c33
Nick Garza <adagar121987@gmail.com>
2019-09-23 19:29:06 -0400

    16 Intercept fetch request and send cached assets (if available) instead)

--- sw.js
+++ sw.js
@@ -8,21 +8,21 @@ const assets = [
     '/js/materialize.min.js',
     '/css/styles.css',
     '/css/materialize.min.css',
-    '/img/dish.png',
+    '/img/clock.png',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+    'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
   ];
 
 // install event
 self.addEventListener('install', evt => {
-    //console.log('service worker installed');
-    evt.waitUntil(
-      caches.open(staticCacheName).then((cache) => {
-        console.log('caching shell assets');
-        cache.addAll(assets);
-      })
-    );
-  });
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
 
 // activate event
 self.addEventListener('activate', evt => {
@@ -32,4 +32,8 @@ self.addEventListener('activate', evt => {
 // fetch event
 self.addEventListener('fetch', evt => {
     // console.log('fetch event', evt);
+    evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request);
+      }))
 });
