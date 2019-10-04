01aa7e98794432b91582148b3cc20a4a1cf3f067
Nick Garza <adagar121987@gmail.com>
2019-10-04 18:49:45 -0400

    Turning hard coded HTML into JS generated

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,60 @@
+const projects = [
+    {
+        skillTitle: "Draw",
+        resource: [
+            "LinkedInLearning - Urban Sketching",
+            "ModernDayJames - Speed Sketching"
+        ],
+        sessions: [
+            {
+                title: "Session 1",
+                description: "Practicing Drawing Noses",
+                date: "1/1/2001",
+                length: "30"
+            },
+            {
+                title: "Session 2",
+                description: "Practicing Drawing Ears",
+                date: "1/1/2001",
+                length: "30"
+            },
+            {
+                title: "Session 3",
+                description: "Practicing Drawing Eyes",
+                date: "1/1/2001",
+                length: "30"
+            }
+        ]
+    },
+    {
+        skillTitle: "Harmonica",
+        resource: [
+            "Heart the Harp",
+            "Blast them notes"
+        ],
+        sessions: [
+            {
+                title: "Session 1",
+                description: "Playing A Key",
+                date: "1/1/2001",
+                length: "30"
+            },
+            {
+                title: "Session 2",
+                description: "Playing B Key",
+                date: "1/1/2001",
+                length: "30"
+            },
+            {
+                title: "Session 3",
+                description: "Playing C Key",
+                date: "1/1/2001",
+                length: "30"
+            }
+        ]
+    }
+]
+
 document.addEventListener('DOMContentLoaded', () => {
     //nav menu
     const menus = document.querySelectorAll('.side-menu');
@@ -7,5 +64,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const forms = document.querySelectorAll('.side-form');
     M.Sidenav.init(forms, {edge: 'left'});
 
+    const body = document.querySelector('.skills.container');
+    body.innerHTML = (projects.map(project => _turnProjectIntoDashboardNode(project)).join(''));
     console.log(forms);
+
+    console.log("DOM CONTENT LOAD COMPLETE");
 })
+
+const _turnProjectIntoDashboardNode = (project) => {
+    console.log(project);
+    let htmlContent = ''
+    htmlContent += `<div class="skill-details">${project.skillTitle}</div>`;
+    return htmlContent;
+}
