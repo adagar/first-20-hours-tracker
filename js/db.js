e2f15a61af309af690d84bb738517a2ccff64995
Nick Garza <adagar121987@gmail.com>
2019-10-21 19:36:08 -0400

    Printing firestore data to the DOM

--- js/db.js
+++ js/db.js
@@ -2,11 +2,13 @@
 db.collection('skill').onSnapshot((snapshot) => {
     // console.log(snapshot.docChanges());
     snapshot.docChanges().forEach(change => {
-        // console.log(change.doc.data(), change.doc.id);
+        console.log(change.doc.data(), change.doc.id);
         if(change.type === 'added'){
             // add doc data to web page
+            renderSkill(change.doc.data(), change.doc.id);
         } else if(change.type === 'removed') {
             // remove doc data from webpage
         }
     });
 })
+
--- js/ui.js
+++ js/ui.js
@@ -1,59 +1,4 @@
-const projects = [
-    {
-        skillTitle: "Draw",
-        resource: [
-            "LinkedInLearning - Urban Sketching",
-            "ModernDayJames - Speed Sketching"
-        ],
-        sessions: [
-            {
-                title: "Session 1",
-                description: "Practicing Drawing Noses",
-                date: "1/1/2001",
-                length: "30"
-            },
-            {
-                title: "Session 2",
-                description: "Practicing Drawing Ears",
-                date: "1/1/2001",
-                length: "30"
-            },
-            {
-                title: "Session 3",
-                description: "Practicing Drawing Eyes",
-                date: "1/1/2001",
-                length: "30"
-            }
-        ]
-    },
-    {
-        skillTitle: "Harmonica",
-        resource: [
-            "Heart the Harp",
-            "Blast them notes"
-        ],
-        sessions: [
-            {
-                title: "Session 1",
-                description: "Playing A Key",
-                date: "1/1/2001",
-                length: "30"
-            },
-            {
-                title: "Session 2",
-                description: "Playing B Key",
-                date: "1/1/2001",
-                length: "30"
-            },
-            {
-                title: "Session 3",
-                description: "Playing C Key",
-                date: "1/1/2001",
-                length: "30"
-            }
-        ]
-    }
-]
+const skills = document.querySelector('.skills');
 
 document.addEventListener('DOMContentLoaded', () => {
     //nav menu
@@ -77,3 +22,46 @@ const _turnProjectIntoDashboardNode = (project) => {
     htmlContent += `<div class="skill-details">${project.skillTitle}</div>`;
     return htmlContent;
 }
+
+// render skill information
+const renderSkill = (data, id) => {
+    let totalTime = 0;
+
+    console.log(data.resources);
+    const resources = data.resources.map(resource => `<li>${resource}</li>`).join('');
+    console.log(resources);
+
+    console.log(data.sessions);
+    const sessions = data.sessions.map(session =>
+        `<li>
+            <div>${session.date}</div>
+            <div>${session.description}</div>
+            <div>${session.length}</div>
+        </li>`).join('');
+
+    const html = `
+        <div class="card-panel skill white row" data-id="${id}">
+            <img src="/img/clock.png" alt="skill thumb">
+            <div class="skill-details">
+                <div class="skill-title">${data.skill}</div>
+                    <div class="skill-resources">
+                        <ul>
+                            ${resources}
+                        </ul>
+                    </div>
+            <div class="skill-sessions">
+                <ul>
+                    ${sessions}
+                </ul>
+            </div>
+            <div class="skill-total">${totalTime}</div>
+            </div>
+            <div class="skill-delete">
+            <i class="material-icons" data-id="${id}">delete_outline</i>
+            </div>
+        </div>
+    `
+
+    skills.innerHTML += html;
+}
+
