4652ece40fb32bf6d97994eafeff1402e851ddae
Nick Garza <adagar121987@gmail.com>
2019-11-25 18:38:21 -0500

    Refactoring App

--- public/index.php
+++ public/index.php
@@ -18,72 +18,10 @@
 </head>
 <body class="yellow lighten-2">
     <?php include "./components/header.html" ?>
-
-    <!-- side nav -->
-    <ul class="sidenav side-menu" id="side-menu">
-        <li><a class="subheader">First 20 Hours - Tracker</a></li>
-        <li><a href="/" class="waves-effect">Home</a></li>
-        <li><a href="/pages/about.html" class="waves-effect">About</a></li>
-        <li><div class="divider"></div></li>
-        <li><a href="/pages/contact.html" class="waves-effect">
-            <i class="material-icons">mail_outlines</i>contact</a>
-        </li>
-    </ul>
-
-    <!--skills-->
-<div class="skills grey-text text-darken-1">
-    <div class="container">
-            <h6 class="center">Skills</h6>
-    </div>
-</div>
-
-    <!--Add Skill Button-->
-    <div class="center">
-        <a class="btn-floating btn-small add-btn sidenav-trigger" data-target="side-form">
-            <i class="material-icons">note_add</i>
-        </a>
-    </div>
-
-    <!--Add skill side nav-->
-    <div id="side-form" class="sidenav side-form">
-        <form class="add-skill container section">
-            <h6>New Skill</h6>
-            <div class="divider"></div>
-                <div class="input-field">
-                    <input placeholder="e.g. Drawing" id="skill-content" type="text" class="validate">
-                    <label for="skill-content">I want to learn:</label>
-                </div>
-                <div class="input-field">
-                    <input placeholder="e.g. Course, Video, Book"id="skill-resource" type="text" class="validate" placeholder="e.g. Class, book, video">
-                    <label for="skill-resource">Resources:</label>
-                </div>
-            <div class="input-field center">
-                <button class="btn-small">Start Learning!</button>
-            </div>
-        </form>
-    </div>
-
-    <!--Add session side nav-->
-    <div id="side-session-form" class="sidenav side-form">
-        <form class="add-session container section">
-            <h6>New Session</h6>
-            <div class="divider"></div>
-            <div>
-                <div class="input-field">
-                    <input id="practice-content" type="text" class="validate">
-                    <label for="practice-content">Today I practiced:</label>
-                </div>
-                <div class="input-field">
-                    <input id="practice-time" type="number" class="validate">
-                    <label for="practice-time">Time Practicing:</label>
-                    <span class="helper-text">Minutes</span>
-                </div>
-            </div>
-            <div class="input-field center">
-                <button class="btn-small">Practice!</button>
-            </div>
-        </form>
-    </div>
+    <?php include "./components/sidenav.html" ?>
+    <?php include "./components/skills.html" ?>
+    <?php include "./components/add-skill.html" ?>
+    <?php include "./components/add-session.html" ?>
 
     <!-- The core Firebase JS SDK is always required and must be listed first -->
 <script src="https://www.gstatic.com/firebasejs/7.2.0/firebase-app.js"></script>
