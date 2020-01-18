0367d89183d45bff61fb123ec3eb4382a1a2f0de
Nick Garza <adagar121987@gmail.com>
2020-01-18 17:57:42 -0500

    Beginning React refactor

---
+++ build/precache-manifest.4beccf033588003f593db1cd428f2efc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ececc23af1df7e588ef5167efa1ff29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eef3e67e745de7f102a6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5aa6a0276fbd2f43a99a",
+    "url": "/static/js/2.bbd2f2f7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bbd2f2f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eef3e67e745de7f102a6",
+    "url": "/static/js/main.6c229027.chunk.js"
+  },
+  {
+    "revision": "d3cf62ab94d4593229c1",
+    "url": "/static/js/runtime-main.1dfb7b6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
